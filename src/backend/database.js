904f96e40324651d8479dc81b3c78df0120bd804
Meraj Sharifi <merajsharifi@Merajs-MacBook-Pro.local>
2024-03-11 14:57:21 +0100

    more edites

--- src/backend/database.js
+++ src/backend/database.js
@@ -2,16 +2,22 @@ require("dotenv").config();
 
 // create connection
 const knex = require("knex")({
-  client: "postgres",
+  client: 'postgres',
   connection: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false },
   },
-  pool: { min: 0, max: 7 },
+  pool: {
+    min: 2,
+    max: 10
+  }
 });
+
+
 
 // Check that the connection works
 knex.raw("SELECT VERSION()").then(() => {
